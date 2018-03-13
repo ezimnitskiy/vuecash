c0084ce7594e33b083b57260bd48f0f3fb572e90
Evgeniy <karpotkin@gmail.com>
2018-03-13 08:03:55 +0300

    Fix count

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -95,7 +95,7 @@ const size_t P2P_LOCAL_WHITE_PEERLIST_LIMIT = 1000;
 const size_t P2P_LOCAL_GRAY_PEERLIST_LIMIT  = 5000;
 
 const size_t P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE        = 32 * 1024 * 1024;  // 32 Mb
-const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT             = 1;
+const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT             = 8;
 const uint32_t P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT = 70;
 const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL            = 60;        // seconds
 const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE               = 50000000;  // 50000000 bytes maximum packet size
